595e291fe35ce456795ed78f13b07151e5e11bf3
ejazali01 <ejaz84916@gmail.com>
2024-05-17 10:17:08 +0530

    Update user.js

--- src/api/auth/user.js
+++ src/api/auth/user.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8080/api/v1"
+axios.defaults.baseURL = "https://damnsruz-backend-1.onrender.com/api/v1"
 
 // login user 
 const userLogin = async (formData) => {
